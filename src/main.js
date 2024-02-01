9f2832cee34a40b6854fb3cf6566cbc79446b7d4
yangweijie <917647288@qq.com>
2024-02-02 00:41:25 +0800

    quasar初始化

--- src/main.js
+++ src/main.js
@@ -1,5 +1,36 @@
 import { createApp } from 'vue'
+import { Quasar } from 'quasar'
 import './style.css'
+import quasarLang from 'quasar/lang/zh-CN'
+
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
+// Assumes your root component is App.vue
+// and placed in same folder as main.js
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const myApp = createApp(App)
+
+myApp.use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
+  lang: quasarLang,
+  /*
+  config: {
+    brand: {
+      // primary: '#e46262',
+      // ... or all other brand colors
+    },
+    notify: {...}, // default set of options for Notify Quasar plugin
+    loading: {...}, // default set of options for Loading Quasar plugin
+    loadingBar: { ... }, // settings for LoadingBar Quasar plugin
+    // ..and many more (check Installation card on each Quasar component/directive/plugin)
+  }
+  */
+})
+
+// Assumes you have a <div id="app"></div> in your index.html
+myApp.mount('#app')
